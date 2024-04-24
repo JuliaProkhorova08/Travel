76ba37e40a03e3d00648134b3eab8900670afe6a
Julia Prokhorova <julia.pro.smm@gmail.com>
2024-04-24 16:00:12 +0300

    Fix header-menu.js, styles for header

--- source/js/header-menu.js
+++ source/js/header-menu.js
@@ -8,11 +8,33 @@ const openMenu = () => {
     if (header.classList.contains('nav--close')) {
       header.classList.remove('nav--close');
       header.classList.add('nav--open');
+      disableScroll(); // Блокировка прокрутки при открытии меню
     } else {
       header.classList.add('nav--close');
       header.classList.remove('nav--open');
+      enableScroll(); // Разблокировка прокрутки при закрытии меню
     }
   });
+
 };
+
+// Блокировка прокрутки страницы при открытом мобильном меню
+function disableScroll() {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollTop}px`;
+  document.body.style.left = `-${scrollLeft}px`;
+}
+
+// Разблокировка прокрутки страницы
+function enableScroll() {
+  const top = parseInt(document.body.style.top, 10);
+  const left = parseInt(document.body.style.left, 10);
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.left = '';
+  window.scrollTo(-left, -top);
+}
 
 export { openMenu };
